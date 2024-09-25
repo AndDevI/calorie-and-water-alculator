f166f14c0565be4401e468415a4fe2c3b939dcdc
Andrew <andrewrgpires@gmail.com>
2024-09-25 15:55:58 -0300

    finish

--- script.js
+++ script.js
@@ -81,6 +81,11 @@ function calculateCalories() {
 
     const water = document.querySelector('#water');
     water.textContent = (weightUser * 0.2).toFixed(2);
+
+
+    const showResults = document.querySelector('#results');
+    showResults.classList.remove("hidden");
+    showResults.classList.toggle("translate-y-0");
 }
 
 const btnCalculateCalories = document.querySelector('button')
