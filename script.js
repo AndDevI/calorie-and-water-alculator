df0620fa7cdc8473ce327d484702450a499116ce
Andrew <andrewrgpires@gmail.com>
2024-09-25 13:40:17 -0300

    creating the function to calculate calories

--- script.js
+++ script.js
@@ -30,18 +30,48 @@ weightInput.addEventListener('input', function() {
     }
 });
 
-const sex = document.querySelectorAll('.sex');
+const sex = document.querySelectorAll('input[name="sex"]');
+let activeSex = null;
 
 sex.forEach((checkbox) => {
     checkbox.addEventListener('change', function() {
         if (this.checked) {
+            activeSex = this.value; 
+
             sex.forEach((otherCheckbox) => {
                 if (otherCheckbox !== this) {
                     otherCheckbox.checked = false;
                 }
             });
+        } else {
+            activeSex = null; 
         }
     });
 });
 
+function calculateCalories() {
+    const heightUser = parseFloat(document.getElementById('height').value);
+    const weightUser = parseFloat(document.getElementById('weight').value);
+    const ageUser = parseFloat(document.getElementById('age').value);
+    const activityFactor = parseFloat(document.getElementById('quantExercise').value); 
 
+    const activeSex = 'male';
+
+    if (isNaN(heightUser) || isNaN(weightUser) || isNaN(ageUser) || isNaN(activityFactor)) {
+        alert("Por favor, insira valores numéricos válidos.");
+        return;
+    }
+
+    let calories;
+    if (activeSex === 'male') {
+        calories = (((13.75 * weightUser) + (5 * heightUser) - (6.76 * ageUser)) + 66.5) * activityFactor;
+    } else {
+        calories = (((9.56 * weightUser) + (1.85 * heightUser) - (4.68 * ageUser)) + 665) * activityFactor;
+    }
+
+    const caloriesNormal = document.querySelector('#caloriesNormal');
+    caloriesNormal.textContent = calories;
+}
+
+const btnCalculateCalories = document.querySelector('button')
+btnCalculateCalories.addEventListener('click', calculateCalories);
